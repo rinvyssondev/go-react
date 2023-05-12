616f5430c49feadcdcc9128d27bd0ce450013e66
Rinvysson <rinvysson.dev@hotmail.com>
2023-05-12 17:53:59 -0300

    subindo

--- db/sqlc/db.go
+++ db/sqlc/db.go
@@ -24,14 +24,32 @@ func New(db DBTX) *Queries {
 func Prepare(ctx context.Context, db DBTX) (*Queries, error) {
 	q := Queries{db: db}
 	var err error
+	if q.createAccountStmt, err = db.PrepareContext(ctx, createAccount); err != nil {
+		return nil, fmt.Errorf("error preparing query CreateAccount: %w", err)
+	}
 	if q.createCategoryStmt, err = db.PrepareContext(ctx, createCategory); err != nil {
 		return nil, fmt.Errorf("error preparing query CreateCategory: %w", err)
 	}
 	if q.createUserStmt, err = db.PrepareContext(ctx, createUser); err != nil {
 		return nil, fmt.Errorf("error preparing query CreateUser: %w", err)
 	}
+	if q.deleteAccountStmt, err = db.PrepareContext(ctx, deleteAccount); err != nil {
+		return nil, fmt.Errorf("error preparing query DeleteAccount: %w", err)
+	}
 	if q.deleteCategoryStmt, err = db.PrepareContext(ctx, deleteCategory); err != nil {
 		return nil, fmt.Errorf("error preparing query DeleteCategory: %w", err)
+	}
+	if q.getAccountStmt, err = db.PrepareContext(ctx, getAccount); err != nil {
+		return nil, fmt.Errorf("error preparing query GetAccount: %w", err)
+	}
+	if q.getAccountsStmt, err = db.PrepareContext(ctx, getAccounts); err != nil {
+		return nil, fmt.Errorf("error preparing query GetAccounts: %w", err)
+	}
+	if q.getAccountsGraphStmt, err = db.PrepareContext(ctx, getAccountsGraph); err != nil {
+		return nil, fmt.Errorf("error preparing query GetAccountsGraph: %w", err)
+	}
+	if q.getAccountsReportsStmt, err = db.PrepareContext(ctx, getAccountsReports); err != nil {
+		return nil, fmt.Errorf("error preparing query GetAccountsReports: %w", err)
 	}
 	if q.getCategoriesStmt, err = db.PrepareContext(ctx, getCategories); err != nil {
 		return nil, fmt.Errorf("error preparing query GetCategories: %w", err)
@@ -45,6 +63,9 @@ func Prepare(ctx context.Context, db DBTX) (*Queries, error) {
 	if q.getUserByIdStmt, err = db.PrepareContext(ctx, getUserById); err != nil {
 		return nil, fmt.Errorf("error preparing query GetUserById: %w", err)
 	}
+	if q.updateAccountStmt, err = db.PrepareContext(ctx, updateAccount); err != nil {
+		return nil, fmt.Errorf("error preparing query UpdateAccount: %w", err)
+	}
 	if q.updateCategoryStmt, err = db.PrepareContext(ctx, updateCategory); err != nil {
 		return nil, fmt.Errorf("error preparing query UpdateCategory: %w", err)
 	}
@@ -53,6 +74,11 @@ func Prepare(ctx context.Context, db DBTX) (*Queries, error) {
 
 func (q *Queries) Close() error {
 	var err error
+	if q.createAccountStmt != nil {
+		if cerr := q.createAccountStmt.Close(); cerr != nil {
+			err = fmt.Errorf("error closing createAccountStmt: %w", cerr)
+		}
+	}
 	if q.createCategoryStmt != nil {
 		if cerr := q.createCategoryStmt.Close(); cerr != nil {
 			err = fmt.Errorf("error closing createCategoryStmt: %w", cerr)
@@ -63,9 +89,34 @@ func (q *Queries) Close() error {
 			err = fmt.Errorf("error closing createUserStmt: %w", cerr)
 		}
 	}
+	if q.deleteAccountStmt != nil {
+		if cerr := q.deleteAccountStmt.Close(); cerr != nil {
+			err = fmt.Errorf("error closing deleteAccountStmt: %w", cerr)
+		}
+	}
 	if q.deleteCategoryStmt != nil {
 		if cerr := q.deleteCategoryStmt.Close(); cerr != nil {
 			err = fmt.Errorf("error closing deleteCategoryStmt: %w", cerr)
+		}
+	}
+	if q.getAccountStmt != nil {
+		if cerr := q.getAccountStmt.Close(); cerr != nil {
+			err = fmt.Errorf("error closing getAccountStmt: %w", cerr)
+		}
+	}
+	if q.getAccountsStmt != nil {
+		if cerr := q.getAccountsStmt.Close(); cerr != nil {
+			err = fmt.Errorf("error closing getAccountsStmt: %w", cerr)
+		}
+	}
+	if q.getAccountsGraphStmt != nil {
+		if cerr := q.getAccountsGraphStmt.Close(); cerr != nil {
+			err = fmt.Errorf("error closing getAccountsGraphStmt: %w", cerr)
+		}
+	}
+	if q.getAccountsReportsStmt != nil {
+		if cerr := q.getAccountsReportsStmt.Close(); cerr != nil {
+			err = fmt.Errorf("error closing getAccountsReportsStmt: %w", cerr)
 		}
 	}
 	if q.getCategoriesStmt != nil {
@@ -86,6 +137,11 @@ func (q *Queries) Close() error {
 	if q.getUserByIdStmt != nil {
 		if cerr := q.getUserByIdStmt.Close(); cerr != nil {
 			err = fmt.Errorf("error closing getUserByIdStmt: %w", cerr)
+		}
+	}
+	if q.updateAccountStmt != nil {
+		if cerr := q.updateAccountStmt.Close(); cerr != nil {
+			err = fmt.Errorf("error closing updateAccountStmt: %w", cerr)
 		}
 	}
 	if q.updateCategoryStmt != nil {
@@ -130,29 +186,43 @@ func (q *Queries) queryRow(ctx context.Context, stmt *sql.Stmt, query string, ar
 }
 
 type Queries struct {
-	db                 DBTX
-	tx                 *sql.Tx
-	createCategoryStmt *sql.Stmt
-	createUserStmt     *sql.Stmt
-	deleteCategoryStmt *sql.Stmt
-	getCategoriesStmt  *sql.Stmt
-	getCategoryStmt    *sql.Stmt
-	getUserStmt        *sql.Stmt
-	getUserByIdStmt    *sql.Stmt
-	updateCategoryStmt *sql.Stmt
+	db                     DBTX
+	tx                     *sql.Tx
+	createAccountStmt      *sql.Stmt
+	createCategoryStmt     *sql.Stmt
+	createUserStmt         *sql.Stmt
+	deleteAccountStmt      *sql.Stmt
+	deleteCategoryStmt     *sql.Stmt
+	getAccountStmt         *sql.Stmt
+	getAccountsStmt        *sql.Stmt
+	getAccountsGraphStmt   *sql.Stmt
+	getAccountsReportsStmt *sql.Stmt
+	getCategoriesStmt      *sql.Stmt
+	getCategoryStmt        *sql.Stmt
+	getUserStmt            *sql.Stmt
+	getUserByIdStmt        *sql.Stmt
+	updateAccountStmt      *sql.Stmt
+	updateCategoryStmt     *sql.Stmt
 }
 
 func (q *Queries) WithTx(tx *sql.Tx) *Queries {
 	return &Queries{
-		db:                 tx,
-		tx:                 tx,
-		createCategoryStmt: q.createCategoryStmt,
-		createUserStmt:     q.createUserStmt,
-		deleteCategoryStmt: q.deleteCategoryStmt,
-		getCategoriesStmt:  q.getCategoriesStmt,
-		getCategoryStmt:    q.getCategoryStmt,
-		getUserStmt:        q.getUserStmt,
-		getUserByIdStmt:    q.getUserByIdStmt,
-		updateCategoryStmt: q.updateCategoryStmt,
+		db:                     tx,
+		tx:                     tx,
+		createAccountStmt:      q.createAccountStmt,
+		createCategoryStmt:     q.createCategoryStmt,
+		createUserStmt:         q.createUserStmt,
+		deleteAccountStmt:      q.deleteAccountStmt,
+		deleteCategoryStmt:     q.deleteCategoryStmt,
+		getAccountStmt:         q.getAccountStmt,
+		getAccountsStmt:        q.getAccountsStmt,
+		getAccountsGraphStmt:   q.getAccountsGraphStmt,
+		getAccountsReportsStmt: q.getAccountsReportsStmt,
+		getCategoriesStmt:      q.getCategoriesStmt,
+		getCategoryStmt:        q.getCategoryStmt,
+		getUserStmt:            q.getUserStmt,
+		getUserByIdStmt:        q.getUserByIdStmt,
+		updateAccountStmt:      q.updateAccountStmt,
+		updateCategoryStmt:     q.updateCategoryStmt,
 	}
 }
--- db/sqlc/querier.go
+++ db/sqlc/querier.go
@@ -9,13 +9,20 @@ import (
 )
 
 type Querier interface {
+	CreateAccount(ctx context.Context, arg CreateAccountParams) (Account, error)
 	CreateCategory(ctx context.Context, arg CreateCategoryParams) (Category, error)
 	CreateUser(ctx context.Context, arg CreateUserParams) (User, error)
+	DeleteAccount(ctx context.Context, id int32) error
 	DeleteCategory(ctx context.Context, id int32) error
+	GetAccount(ctx context.Context, id int32) (Account, error)
+	GetAccounts(ctx context.Context, arg GetAccountsParams) ([]GetAccountsRow, error)
+	GetAccountsGraph(ctx context.Context, arg GetAccountsGraphParams) (int64, error)
+	GetAccountsReports(ctx context.Context, arg GetAccountsReportsParams) (int64, error)
 	GetCategories(ctx context.Context, arg GetCategoriesParams) ([]Category, error)
 	GetCategory(ctx context.Context, id int32) (Category, error)
 	GetUser(ctx context.Context, username string) (User, error)
 	GetUserById(ctx context.Context, id int32) (User, error)
+	UpdateAccount(ctx context.Context, arg UpdateAccountParams) (Account, error)
 	UpdateCategory(ctx context.Context, arg UpdateCategoryParams) (Category, error)
 }
 
